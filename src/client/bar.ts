c3317d6c3e12faa54f77b94830679308883bed05
Vladislav Veluga <vladislav805@yandex.com>
2020-06-17 23:49:10 +0300

    Rename variables

--- src/client/bar.ts
+++ src/client/bar.ts
@@ -2,14 +2,14 @@ import { IPositionRecord } from '../types';
 import { getHumanTimeDiff, getSpeedByInterpolate } from './utils';
 import { getUnixTime } from '../utils';
 
-let speed: HTMLElement;
-let latency: HTMLElement;
+let nodeSpeed: HTMLElement;
+let nodeLatency: HTMLElement;
 
 let previousPosition: IPositionRecord;
 
 export const initBar = (): void => {
-    speed = document.getElementById('speed');
-    latency = document.getElementById('latency');
+    nodeSpeed = document.getElementById('speed');
+    nodeLatency = document.getElementById('latency');
     setInterval(updateLatency, 1000);
 };
 
@@ -17,15 +17,15 @@ export const setBarInfo = (position: IPositionRecord): void => {
     const kmPh = previousPosition
         ? getSpeedByInterpolate(position, previousPosition)
         : position.speed;
-    speed.textContent = String(kmPh);
-    latency.dataset.time = String(position.time);
+    nodeSpeed.textContent = String(kmPh);
+    nodeLatency.dataset.time = String(position.time);
     previousPosition = position;
 };
 
 const updateLatency = (): void => {
     const now = getUnixTime();
-    if (latency.dataset.time) {
-        const diff = now - +latency.dataset.time;
-        latency.textContent = diff ? `${getHumanTimeDiff(diff)} ago` : 'just now';
+    if (nodeLatency.dataset.time) {
+        const diff = now - +nodeLatency.dataset.time;
+        nodeLatency.textContent = diff ? `${getHumanTimeDiff(diff)} ago` : 'just now';
     }
 };
